18d65a02064a4653ce4fe37c25d837e47c819afd
chris <>
2020-12-28 18:22:48 +0900

    add project

---
+++ springboot/src/main/java/davenkin/HelloWorld.java
@@ -0,0 +1,10 @@
+package davenkin;
+
+import org.springframework.stereotype.Component;
+@Component
+public class HelloWorld {
+
+    public String hello() {
+        return "Hello My World!";
+    }
+}
