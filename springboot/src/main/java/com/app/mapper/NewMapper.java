0ea4ac618b46a9b72fdf7ed8c152c946bf39e16f
chris <>
2021-02-04 19:37:34 +0900

    2021-02-04 19:37:33

--- springboot/src/main/java/com/app/mapper/NewMapper.java
+++ springboot/src/main/java/com/app/mapper/NewMapper.java
@@ -22,7 +22,7 @@ public interface NewMapper{
     @Select("select * from news.contents where newid = #{id}")
     List<Content> findContentByNewID(String id);
 
-    @Select("select * from news.news where type ='${type}' and id < #{id} and needshow = 1 order by time desc, id desc limit 20")
+    @Select("select * from news.news where type ='${type}' and id < #{id} and needshow = 1 and imgurl != 'https://cdn.mainichi.jp/vol1/images/icon/mainichi/ogp.png' order by time desc, id desc limit 20")
     // @Results({
     //     @Result(
     //         property="contents",
@@ -32,7 +32,7 @@ public interface NewMapper{
     List<News> selectNextPageByIDAndType(int id,@Param("type") String type);
 
 
-    @Select("select * from news.news where type ='${type}' and needshow = 1 order by time desc, id desc limit 20")
+    @Select("select * from news.news where type ='${type}' and needshow = 1 and imgurl != 'https://cdn.mainichi.jp/vol1/images/icon/mainichi/ogp.png' order by time desc, id desc limit 20")
     // @Results({
     //     @Result(
     //         property="contents",
@@ -41,7 +41,7 @@ public interface NewMapper{
     // })
     List<News> selectFirstPageByType(@Param("type") String type);
 
-    @Select("select count(*) from news.news where type ='${type}' and needshow = 1 order by time desc, id desc")
+    @Select("select count(*) from news.news where type ='${type}' and needshow = 1 and imgurl != 'https://cdn.mainichi.jp/vol1/images/icon/mainichi/ogp.png' order by time desc, id desc")
     int selectCountByType(@Param("type") String type);
 
 
